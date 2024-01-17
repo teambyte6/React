2f938d2a382a2c80b9e5bd117137c2a4327b0291
Dinesh <tdinesh1608@gmail.com>
2024-01-17 17:12:08 +0530

    conversion of currency

---
+++ currencygenerator/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{backgroundImage:`url(https://images.pexels.com/photos/1447418/pexels-photo-1447418.jpeg?auto=compress&cs=tinysrgb&w=600)` }}>
+      <h1 className="text-3xl font-bold underline text-orange-600 text-center my-8">React Currency Generator</h1>
+    </div>
+  );
+}
+
+export default App;
